e1f87d9146c23109ae8c020550ef4abaf650d8a3
Elaine Vuong <elaine.yt.vuong@gmail.com>
2022-08-16 08:52:19 -0400

    Updated Provider import from react-redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-import store from './store'
-import { Provider } from 'react';
+import store from "./lib/store"
+console.log(store)
+
 
 ReactDOM.render(
   <React.StrictMode>
